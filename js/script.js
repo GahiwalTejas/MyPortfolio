8a1da675839ecd5b6d90ced64546455092b900a8
GahiwalTejas <tejasgahiwal1999@gmail.com>
2024-04-11 12:20:49 +0530

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// typed script
+var typed = new Typed("#element", {
+    strings: ["Web Developer", "Web Designer"],
+    typeSpeed: 100,
+  });
+
+  // aos script
+  AOS.init({
+    offset: 250,
+    duration: 1000,
+  });
